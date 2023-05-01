e8399a59346f643da3cab05673498b4960644181
Min Koo <mkoo@c8r8s3.42seoul.kr>
2023-05-01 23:39:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pyperclip
 import threading 
 import time
 
-openai.api_key ="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"
+openai.api_key =""
 
 def pdf_to_text(pdf_path):
 	document = fitz.open(pdf_path)
@@ -19,7 +19,9 @@ def choose_file():
 	filepath = filedialog.askopenfilename()
 	if filepath.endswith('.pdf'):
 		file_path_var.set(filepath)
-	# 	output_text.delete(1.0, tk.END)
+		summary_text.delete(1.0, tk.END)
+		translate_text.delete(1.0, tk.END)
+		exercise_text.delete(1.0, tk.END)
 	else:
 		messagebox.showerror(title = 'Error', message = 'Please select valid file extension.')
 
@@ -31,7 +33,7 @@ def generate_summary(text):
         model="text-davinci-003",
         prompt=f"Summarize this: {prompt}",
         temperature=0.9,
-        max_tokens=256,
+        max_tokens=1000,
         top_p=1.0,
         frequency_penalty=0.0,
         presence_penalty=0.0
@@ -41,13 +43,13 @@ def generate_summary(text):
 
 def generate_translate(text):
 	words = text.split()
-	max_words = 1000
+	max_words = 2000
 	prompt = " ".join(words[:max_words])
 	response = openai.Completion.create(
         model="text-davinci-003",
         prompt=f"Translate this in Korean: {prompt}", #Korean -> Other Languages
         temperature=0.9,
-        max_tokens=1000,
+        max_tokens=2000,
         top_p=1.0,
         frequency_penalty=0.0,
         presence_penalty=0.0
@@ -77,7 +79,6 @@ def execute_summary():
 		text = pdf_to_text(file_path)
 		summary = generate_summary(text)
 		summary_text.insert(tk.END, summary)
-		text_summary.set(summary)
 	else:
 		messagebox.showerror(title = 'Error', message = 'Please select file.')
 
@@ -86,7 +87,7 @@ def execute_translate():
 	if file_path:
 		text = pdf_to_text(file_path)
 		translate = generate_translate(text)
-		text_translate.set(translate)
+		translate_text.insert(tk.END, translate)
 	else:
 		messagebox.showerror(title = 'Error', message = 'Please select file.')
 
@@ -95,7 +96,7 @@ def execute_question():
 	if file_path:
 		text = pdf_to_text(file_path)
 		question = generate_question(text)
-		text_question.set(question)
+		exercise_text.insert(tk.END, question)
 	else:
 		messagebox.showerror(title = 'Error', message = 'Please select file.')
         
@@ -125,7 +126,7 @@ text_question = tk.StringVar()
 file_path_var = tk.StringVar()
 
 window.title("Edu-GPT")
-window.geometry("600x1200")
+window.geometry("600x1400")
 window.resizable(False, True)
 
 font1 = font.Font(family="arial", size=20)
@@ -138,14 +139,14 @@ paned_window1=tk.PanedWindow(relief="raised")
 paned_window1.pack(side="top")
 
 head_input = tk.Label(window, text="File : ",font=font1)
-name_input = tk.Label(window, text="iphone.pdf",font=font1)
+name_input = tk.Label(window, textvariable = file_path_var, font=font1)
 button_input = tk.Button(window, overrelief="solid",width=15,text="browse file", command=lambda:choose_file())
 paned_window1.add(head_input)
 paned_window1.add(name_input)
 paned_window1.add(button_input)
 paned_window1.pack(side="top",pady=10)
 
-
+#--------------------summary-------------------------
 paned_window2=tk.PanedWindow(relief="raised")
 paned_window2.pack(side="top",pady=10)
 button_summary = tk.Button(window, overrelief="solid",width=15,text="Generate Summary",pady=3,command=lambda:execute_summary())
@@ -158,7 +159,7 @@ summary_frame.pack(side="top",pady=10)
 
 button_save = tk.Button(summary_frame, text="save",height=0)
 button_copy = tk.Button(summary_frame, text="copy",height=0)
-button_reset = tk.Button(summary_frame, text="reset",height=0)
+button_refresh = tk.Button(summary_frame, text="refresh",height=0)
 
 # summary_text = tk.Label(summary_frame, textvariable=text_summary)
 summary_text = tk.Text(summary_frame,width=76, height=10)
@@ -167,13 +168,39 @@ summary_head = tk.Label(summary_frame, text="summary", font=font1, fg="gray")
 summary_head.place(x=0,y=3)
 button_save.place(x=410,y=5)
 button_copy.place(x=450,y=5)
-button_reset.place(x=490,y=5)
+button_refresh.place(x=490,y=5)
 summary_text.place(x=0,y=30)
 
+#--------------------translate--------------------
+paned_window5=tk.PanedWindow(relief="raised")
+paned_window5.pack(side="top",pady=10)
+
+button_translate = tk.Button(window, overrelief="solid",width=15,text="Generate Translate",pady=3,command=lambda:execute_translate())
+label_translate_progress = tk.Label(window, text="processing...",height=0,fg="gray")
+paned_window5.add(button_translate)
+paned_window5.add(label_translate_progress)
+
+translate_frame = tk.Frame(window, relief="solid",width = 550,height=200,bd=2,padx=0)
+translate_frame.pack(side="top",pady=10)
+
+button_save = tk.Button(translate_frame, text="save", height=0)
+button_copy = tk.Button(translate_frame, text="copy", height=0)
+button_refresh = tk.Button(translate_frame, text="refresh", height=0)
+
+translate_text = tk.Text(translate_frame, width=76, height=10)
+translate_head = tk.Label(translate_frame, text="translate", font=font1, fg="gray")
+
+translate_head.place(x=0, y=3)
+button_save.place(x=410, y=5)
+button_copy.place(x=450, y=5)
+button_refresh.place(x=490, y=5)
+translate_text.place(x=0, y=30)
+
+#--------------------exercise--------------------
 paned_window4=tk.PanedWindow(relief="raised")
 paned_window4.pack(side="top",pady=10)
 
-button_exercise = tk.Button(window, overrelief="solid",width=15,text="Generate Exercise",pady=3)
+button_exercise = tk.Button(window, overrelief="solid",width=15,text="Generate Exercise", pady=3, command=lambda:execute_question())
 label_exercise_progress = tk.Label(window, text="processing...",height=0,fg="gray")
 paned_window4.add(button_exercise)
 paned_window4.add(label_exercise_progress)
@@ -183,15 +210,15 @@ exercise_frame.pack(side="top",pady=10)
 
 button_save = tk.Button(exercise_frame, text="save",height=0)
 button_copy = tk.Button(exercise_frame, text="copy",height=0)
-button_reset = tk.Button(exercise_frame, text="reset",height=0)
+button_refresh = tk.Button(exercise_frame, text="refresh",height=0)
 
-exercise_text = tk.Label(exercise_frame, textvariable=text_question)
+exercise_text = tk.Text(exercise_frame, width=76, height=10)
 exercise_head = tk.Label(exercise_frame, text="exercise", font=font1, fg="gray")
 
 exercise_head.place(x=0,y=3)
 button_save.place(x=410,y=5)
 button_copy.place(x=450,y=5)
-button_reset.place(x=490,y=5)
+button_refresh.place(x=490,y=5)
 exercise_text.place(x=0,y=30)
 
 paned_window3=tk.PanedWindow(relief="raised")
@@ -207,18 +234,17 @@ answer_frame.pack(side="top",pady=10)
 
 button_save = tk.Button(answer_frame, text="save", height=0)
 button_copy = tk.Button(answer_frame, text="copy", height=0)
-button_reset = tk.Button(answer_frame, text="reset", height=0)
+button_refresh = tk.Button(answer_frame, text="refresh", height=0)
 
-answer_text = tk.Label(answer_frame, text="ahiuhiufhaiu")
+answer_text = tk.Text(answer_frame, width=76, height=10)
 answer_head = tk.Label(answer_frame, text="answer", font=font1, fg="gray")
 
 answer_head.place(x=0, y=3)
 button_save.place(x=410, y=5)
 button_copy.place(x=450, y=5)
-button_reset.place(x=490, y=5)
+button_refresh.place(x=490, y=5)
 answer_text.place(x=0, y=30)
 
-label_summary_output = tk.Label(window, text="processing...", height=0, fg="gray")
 window.mainloop()
 #---------------------gui----------------------
 
